d5f5b95997b760e1f4d375f06fcffae966da3eab
marielle612 <jsjang612@gmail.com>
2025-03-23 02:10:44 +0900

    multivariate version PCM

--- test1.R
+++ test1.R
@@ -277,5 +277,260 @@ dr.M.rrr<-function(object, nslices=2, dy=2, nclust=4, minc=5, slice.info=NULL,..
   return(list(M = M.pr, slice.info = kmeans.out))
 }
 
+##
+## Multiveraite version of PCM
+##
+#######################################################################
+#######################################################################
+
+#######################################################################
+## Pooled PCM + Projection Resampling PCM
+#######################################################################
+dr.M.PRpoolpcm<-function(object, nslices=2, nclust=4, pr.iter=100, minc=5, slice.info=NULL,...) {
+  z <- dr.z(object)
+  x <- dr.x(object)
+  y <- dr.y(object)
+  wts <- dr.wts(object)
+  
+  r <- dim(y)[2]
+  p <- dim(z)[2]
+  
+  ## Stating PRpcm
+  M <-0
+  
+  for (i in 1:pr.iter){ 
+    t.i <- as.matrix(rnorm(r))
+    t.i <- t.i /sqrt(sum(t.i^2))
+    t.i.y <- c(y %*% t.i)
+    ols1 <- dr.directions(dr( t.i.y~x, method="ols"))[,1]
+    phd1 <- dr.directions(dr( t.i.y~x, method="phdres"))[,1]
+    PI <- cbind(ols1, phd1)  
+    
+    if (nclust == 4) { kmeans.out <- dr.slices(PI, nslices=c(2,2))
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx) }  else { kmeans.out <- kmeans.min(PI, k=nclust, min=minc)
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx)   }
+    
+    M.j <- NULL
+    for(k in 1:actual.k){
+      sel.k <- C.x==k
+      z.k <- z[sel.k, ] ;  y.k <- t.i.y[sel.k]
+      wts.k <- wts[sel.k]
+      
+      slices <- dr.slices.arc(y.k, nslices)
+      zmeans <- matrix(0,slices$nslices,NCOL(z.k))
+      slice.weight <- rep(0,slices$nslices)  # NOT rep(0,NCOL(z.k))
+      
+      # compute weighted means within slice 
+      wmean <- function (x, wts) { sum(x * wts) / sum (wts) }
+      for (j in 1:slices$nslices){
+        sel <- slices$slice.indicator==j
+        zmeans[j,]<- apply(z.k[sel,],2, wmean, wts.k[sel])
+        slice.weight[j]<-sum(wts.k[sel])}
+      
+      # get M matrix for sir
+      ini.M <- t(apply(zmeans, 2, "*", sqrt(slice.weight))/ sqrt(sum(slice.weight)))
+      M.j <- cbind(M.j, ( ini.M*(size.Cx[k]/total.Cx) ))
+    } ## coordinate mean
+    
+    ##    if (dim(M.j)[2] != (nslices*nclust) ) {
+    ##         diff.col <-  (nslices*nclust) - dim(M.j)[2]
+    ##         M.j.zeros <- matrix( rep(0, (p*diff.col) ), c(p, diff.col) )
+    ##         M.j <- cbind(M.j, M.j.zeros) }
+    
+    M.j <- M.j%*% t(M.j)
+    M <- M+M.j
+  }
+  M.pr <- M/pr.iter
+  
+  ## Starting pooled pcm
+  M <-0
+  
+  for (i in 1:r){ 
+    ols1 <- dr.directions(dr(y[,i]~x, method="ols"))[,1]
+    phd1 <- dr.directions(dr(y[,i]~x, method="phdres"))[,1]
+    PI <- cbind(ols1, phd1)  
+    
+    if (nclust == 4) { kmeans.out <- dr.slices(PI, nslices=c(2,2))
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx) }  else { kmeans.out <- kmeans.min(PI, k=nclust, min=minc)
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx)   }
+    
+    
+    ## Start coordinate mean
+    M.j <- NULL
+    
+    for(k in 1:actual.k){
+      sel.k <- C.x==k
+      z.k <- z[sel.k, ] ;  y.k <- y[sel.k, i]
+      wts.k <- wts[sel.k]
+      
+      slices <- dr.slices(y.k, nslices)
+      zmeans <- matrix(0,slices$nslices,NCOL(z.k))
+      slice.weight <- rep(0,slices$nslices)  # NOT rep(0,NCOL(z.k))
+      
+      # compute weighted means within slice 
+      wmean <- function (x, wts) { sum(x * wts) / sum (wts) }
+      for (j in 1:slices$nslices){
+        sel <- slices$slice.indicator==j
+        zmeans[j,]<- apply(z.k[sel, ],2, wmean, wts.k[sel])
+        slice.weight[j]<-sum(wts.k[sel])}
+      
+      # get M matrix for sir
+      ini.M <- t(apply(zmeans, 2, "*", sqrt(slice.weight))/ sqrt(sum(slice.weight)))
+      M.j <- cbind(M.j, ( ini.M*(size.Cx[k]/total.Cx) ))
+    } ## End coordinate mean
+    
+    M.j <- M.j%*% t(M.j)
+    M <- M+M.j
+  }## End for corrdinate regression
+  
+  M.pool <- M/r
+  
+  ## Summing PRpcm + pooled pcm
+  M <- (M.pool+M.pr)*0.5
+  
+  return(list(M = M, slice.info = kmeans.out))
+}
+
+
+
+#######################################################################
+## Pooled PCM
+#######################################################################
+dr.M.poolpcm<-function(object, nslices=2, nclust=4, minc=10, slice.info=NULL,...) {
+  z <- dr.z(object);   x <- dr.x(object)
+  y <- dr.y(object);    wts <- dr.wts(object)
+  
+  r <- dim(y)[2];     p <- dim(z)[2]
+  
+  M <-0
+  
+  for (i in 1:r){ 
+    ols1 <- dr.directions(dr(y[,i]~x, method="ols"))[,1]
+    phd1 <- dr.directions(dr(y[,i]~x, method="phdres"))[,1]
+    PI <- cbind(ols1, phd1)  
+    
+    if (nclust == 4) { kmeans.out <- dr.slices(PI, nslices=c(2,2))
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx) }  else { kmeans.out <- kmeans.min(PI, k=nclust, min=minc)
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx)   }
+    
+    
+    ## Start coordinate mean
+    M.j <- NULL
+    
+    for(k in 1:actual.k){
+      sel.k <- C.x==k
+      z.k <- z[sel.k, ] ;  y.k <- y[sel.k, i]
+      wts.k <- wts[sel.k]
+      
+      slices <- dr.slices(y.k, nslices)
+      zmeans <- matrix(0,slices$nslices,NCOL(z.k))
+      slice.weight <- rep(0,slices$nslices)  # NOT rep(0,NCOL(z.k))
+      
+      # compute weighted means within slice 
+      wmean <- function (x, wts) { sum(x * wts) / sum (wts) }
+      for (j in 1:slices$nslices){
+        sel <- slices$slice.indicator==j
+        zmeans[j,]<- apply(z.k[sel, ],2, wmean, wts.k[sel])
+        slice.weight[j]<-sum(wts.k[sel])}
+      
+      # get M matrix for sir
+      ini.M <- t(apply(zmeans, 2, "*", sqrt(slice.weight))/ sqrt(sum(slice.weight)))
+      M.j <- cbind(M.j, ( ini.M*(size.Cx[k]/total.Cx) ))
+    } ## End coordinate mean
+    
+    M.j <- M.j%*% t(M.j)
+    M <- M+M.j
+  }## End for corrdinate regression
+  
+  M <- M / r
+  
+  return(list(M = M, slice.info = kmeans.out))
+}
+
+#######################################################################
+## Projection Resampling PSM
+#######################################################################
+dr.M.PRpcm<-function(object, nslices=2, nclust=4, pr.iter=1000, minc=5, slice.info=NULL,...) {
+  z <- dr.z(object)
+  x <- dr.x(object)
+  y <- dr.y(object)
+  wts <- dr.wts(object)
+  
+  r <- dim(y)[2]
+  p <- dim(z)[2]
+  
+  M <-0
+  
+  for (i in 1:pr.iter){ 
+    t.i <- as.matrix(rnorm(r))
+    t.i <- t.i /sqrt(sum(t.i^2))
+    t.i.y <- c(y %*% t.i)
+    ols1 <- dr.directions(dr( t.i.y~x, method="ols"))[,1]
+    phd1 <- dr.directions(dr( t.i.y~x, method="phdres"))[,1]
+    PI <- cbind(ols1, phd1)  
+    
+    if (nclust == 4) { kmeans.out <- dr.slices(PI, nslices=c(2,2))
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx) }  else { kmeans.out <- kmeans.min(PI, k=nclust, min=minc)
+    actual.k <- kmeans.out$nslices
+    C.x <- kmeans.out$slice.indicator
+    size.Cx <- kmeans.out$slice.sizes
+    total.Cx <- sum(size.Cx)   }
+    
+    M.j <- NULL
+    for(k in 1:actual.k){
+      sel.k <- C.x==k
+      z.k <- z[sel.k, ] ;  y.k <- t.i.y[sel.k]
+      wts.k <- wts[sel.k]
+      
+      slices <- dr.slices.arc(y.k, nslices)
+      zmeans <- matrix(0,slices$nslices,NCOL(z.k))
+      slice.weight <- rep(0,slices$nslices)  # NOT rep(0,NCOL(z.k))
+      
+      # compute weighted means within slice 
+      wmean <- function (x, wts) { sum(x * wts) / sum (wts) }
+      for (j in 1:slices$nslices){
+        sel <- slices$slice.indicator==j
+        zmeans[j,]<- apply(z.k[sel,],2, wmean, wts.k[sel])
+        slice.weight[j]<-sum(wts.k[sel])}
+      
+      # get M matrix for sir
+      ini.M <- t(apply(zmeans, 2, "*", sqrt(slice.weight))/ sqrt(sum(slice.weight)))
+      M.j <- cbind(M.j, ( ini.M*(size.Cx[k]/total.Cx) ))
+    } ## coordinate mean
+    
+    ##    if (dim(M.j)[2] != (nslices*nclust) ) {
+    ##         diff.col <-  (nslices*nclust) - dim(M.j)[2]
+    ##         M.j.zeros <- matrix( rep(0, (p*diff.col) ), c(p, diff.col) )
+    ##         M.j <- cbind(M.j, M.j.zeros) }
+    
+    M.j <- M.j%*% t(M.j)
+    M <- M+M.j
+  }
+  
+  M <- M/pr.iter
+  
+  return(list(M = M, slice.info = kmeans.out))
+}
 
 
